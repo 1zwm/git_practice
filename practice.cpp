6a811e2a363199de6c73d360ddf7f8350ac784ac
zwm <1273764385@qq.com>
2021-10-14 23:22:54 +0800

    1

--- practice.cpp
+++ practice.cpp
@@ -1,23 +1,52 @@
-/*
- * @Author: your name
- * @Date: 2021-10-14 16:11:52
- * @LastEditTime: 2021-10-14 16:37:50
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: /practice/practice.cpp
- */
-
+#include <string>
+#include <cstdio>
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
 int main() {
-    int sum = 0;
-    int value = 0;
-    cout << "please input data ! " << endl;
-    while(cin >> value) {
-        sum += value;
+    int n = 0;
+    cin >> n;
+    string temp; //input string
+    vector<string> ans; //answer
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        string str = "";
+        str += temp.substr(0, 2);
+        int j = 2;
+        while(j < int(temp.size())) {
+            if(temp[j-2] == temp[j-1]) {
+                if(temp[j] == temp[j-1]) {
+                    char c = temp[j];
+                    while(j+1 < int(temp.size()) && temp[++j] == c);
+                    if(temp[j] != c)
+                        str.push_back(temp[j]);
+                    c = temp[j];
+                    while(j+1 < int(temp.size()) && temp[++j] == c);
+                    if(temp[j] != c)
+                        str.push_back(temp[j]);
+                    j++;
+                 }
+                else if(j+1 < int(temp.length()) && temp[j] == temp[j+1]) {
+                        str.push_back(temp[j]);
+                        char c = temp[j];
+                            j++;
+                        while(j+1 < int(temp.size()) && temp[++j] == c);
+                        if(temp[j] != c)
+                            str.push_back(temp[j]);
+                        j++;
+                    }
+                else {
+                    str.push_back(temp[j]);
+                    j++;
+                }
+            } else {
+                str.push_back(temp[j]);
+                j++;
+            }
+        }
+        ans.emplace_back(str);
+        }
+        for(int i = 0; i < int(ans.size()); i++) {
+            cout << ans[i] << endl;
+        }
     }
-    cout << "The sum is : " << sum << endl;
-    return 0;
-}
--- test.cpp
+++ test.cpp
@@ -1,8 +1,61 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-14 16:24:05
+ * @LastEditTime: 2021-10-14 23:04:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /practice/test.cpp
+ */
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+typedef unsigned int ui;
+
+void exgcd(ui a, ui b, ui& d, ui& x, ui& y) {
+    if(b == 0) {
+        d = a;
+        x = 1;
+        y = 0;
+    } else {
+        exgcd(b, a%b, d, y, x);
+        y -= x * (a/b);
+    }
+}
+
+ui inv(ui a, ui b) {
+    ui d, x, y;
+    exgcd(a, b, d, x, y);
+    return d == 1 ? (x+b)%b : -1;
+}
+
+ui factory(int n) {
+    ui ans = 1;
+    for(int i = 1; i <= n; i++) {
+        ans *= i;
+    }
+    return ans % 99997867;
+}
+
 int main() {
-    cout << "Hello World !" << endl;
+    int N, D;
+    ui ans = 0;
+    cin >> N >> D;
+    int building[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &building[i]);
+    }
+    int end = 1;
+    for(int i = 0; i < N; i++) {
+        while(end < N && building[i] + D >= building[end]) end++;
+        if(end - i - 1 > 1) {
+            ui temp = inv(factory(end - i - 3) * 2, 99997867);
+            ans += factory(end - i - 1) * temp;
+            ans = ans % 99997867;
+        }
+    }
+    cout << ans;
     return 0;
 }
